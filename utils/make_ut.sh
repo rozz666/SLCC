c43c756dc4ef9470a67a8321fc653a040b83ef42
Rafal Przywarski <rp@rp-PC.(none)>
2010-05-02 18:21:58 +0200

    Fixed UT template generator

--- utils/make_ut.sh
+++ utils/make_ut.sh
@@ -1,9 +1,41 @@
 #!/usr/bin/bash
-if [ ! "$#" = "2" ]
-then
+if [ ! "$#" = "2" ]; then
     echo "usage: make_ut.sh <name> <desc>"
-
     exit 1
 fi
+NAME="$1"
+DESC="$2"
+CODE="#include <tut/tut.hpp> 
 
-cat ut_template | sed -e "s/\$(NAME)/$1/" | sed -e "s/\$(DESC)/$2/" > "$1_ut.cpp"
+namespace tut
+{
+
+struct ${NAME}_Test_data
+{
+};
+
+typedef test_group<${NAME}_Test_data> tg;
+typedef tg::object object;
+tg ${NAME}_group(\"$DESC\");
+
+template <>
+template <>
+void object::test<1>()
+{
+}
+
+template <>
+template <>
+void object::test<2>()
+{
+}
+
+template <>
+template <>
+void object::test<3>()
+{
+}
+
+}"
+
+echo "$CODE" > "${NAME}_ut.cpp"
