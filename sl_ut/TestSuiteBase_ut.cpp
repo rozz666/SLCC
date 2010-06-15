9ce8c744c16d93e8d5a0da4d25ccae2a716f0ef3
U-NSN-INTRA\przywars <przywars@7SB6B4J.nsn-intra.net>
2010-06-15 07:46:19 +0200

    Some test framework UTs

---
+++ sl_ut/TestSuiteBase_ut.cpp
@@ -0,0 +1,77 @@
+#include <tut/tut.hpp>
+#include <sl/test/TestSuiteBase.hpp>
+
+namespace tut
+{
+
+struct TestSuiteBase_Test_data
+{
+    class TestTestSuite : public sl::test::TestSuiteBase
+    {
+    public:
+
+        TestTestSuite(unsigned N, const char *name) : sl::test::TestSuiteBase(N, name) { }
+
+        virtual void runTests(sl::test::Reporter& reporter)
+        {
+            setReporter(reporter);
+        }
+
+        using sl::test::TestSuiteBase::setReporter;
+    };
+
+    class ReporterStub : public sl::test::Reporter
+    {
+    public:
+
+        std::vector<std::string> runTestCalls;
+
+        virtual void run() { }
+        virtual void finish() { }
+        virtual void runTestSuite(unsigned id, const std::string& name) { }
+        virtual void finishTestSuite() { }
+        virtual void runTest(const std::string& name) { runTestCalls.push_back(name); }
+        virtual void finishTest() { }
+        virtual void outputMismatch(const std::string& actual, const std::string& expected) { }
+        virtual void compilationError(const std::string& msg) { }
+        virtual void errorMissing(const std::string& expected) { }
+        virtual void unexpectedError(const std::string& expected) { }
+        virtual void fileNotFound(const std::string& name) { }
+    };
+};
+
+typedef test_group<TestSuiteBase_Test_data> tg;
+typedef tg::object object;
+tg TestSuiteBase_group("TestSuiteBase");
+
+template <>
+template <>
+void object::test<1>()
+{
+    TestTestSuite tts(1u, "SuiteName");
+    ReporterStub reporter;
+
+    ensure_equals("number", tts.number(), 1u);
+    ensure_equals("name", tts.name(), "SuiteName");
+
+    tts.setReporter(reporter);
+
+    tts.test("test", "test.sl");
+
+    ensure_equals("call", reporter.runTestCalls.size(), 1u);
+    ensure_equals("name", reporter.runTestCalls[0], "test");
+}
+
+template <>
+template <>
+void object::test<2>()
+{
+}
+
+template <>
+template <>
+void object::test<3>()
+{
+}
+
+}
