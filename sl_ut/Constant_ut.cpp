f0066341098435314766fa9e05ac0685a90d0fb8
Rafal Przywarski <rp@rp-PC.(none)>
2010-06-20 18:21:41 +0200

    Constant and Variable UTs

---
+++ sl_ut/Constant_ut.cpp
@@ -0,0 +1,54 @@
+#include <tut/tut.hpp> 
+#include <sl/ast/Constant.hpp>
+
+namespace tut
+{
+
+struct Constant_Test_data
+{
+};
+
+typedef test_group<Constant_Test_data> tg;
+typedef tg::object object;
+tg Constant_group("Constant");
+
+template <>
+template <>
+void object::test<1>()
+{
+    sl::FilePosition pos("xxx", 1, 2);
+    sl::int_t val(10);
+    sl::ast::Constant c(pos, val);
+
+    ensure_equals("pos", c.pos(), pos);
+    ensure("value", c.value() == sl::ast::Constant::Value(val));
+    ensure_equals("type", c.type(), sl::int_);
+}
+
+template <>
+template <>
+void object::test<2>()
+{
+    sl::FilePosition pos("xxx", 1, 2);
+    sl::float_t val(10.0f);
+    sl::ast::Constant c(pos, val);
+
+    ensure_equals("pos", c.pos(), pos);
+    ensure("value", c.value() == sl::ast::Constant::Value(val));
+    ensure_equals("type", c.type(), sl::float_);
+}
+
+template <>
+template <>
+void object::test<3>()
+{
+    sl::FilePosition pos("xxx", 1, 2);
+    bool val(false);
+    sl::ast::Constant c(pos, val);
+
+    ensure_equals("pos", c.pos(), pos);
+    ensure("value", c.value() == sl::ast::Constant::Value(val));
+    ensure_equals("type", c.type(), sl::bool_);
+}
+
+}
---
+++ sl_ut/Variable_ut.cpp
@@ -0,0 +1,29 @@
+#include <tut/tut.hpp>
+#include <sl/ast/Variable.hpp>
+
+namespace tut
+{
+
+struct Variable_Test_data
+{
+};
+
+typedef test_group<Variable_Test_data> tg;
+typedef tg::object object;
+tg Variable_group("Variable");
+
+template <>
+template <>
+void object::test<1>()
+{
+    std::string name("x");
+    sl::FilePosition pos("xxx", 3, 4);
+    sl::ast::Variable v(name, pos, sl::int_, false);
+
+    ensure_equals("name", v.name(), name);
+    ensure_equals("pos", v.pos(), pos);
+    ensure_equals("type", v.type(), sl::int_);
+    ensure_equals("ref", v.ref(), false);
+}
+
+}
